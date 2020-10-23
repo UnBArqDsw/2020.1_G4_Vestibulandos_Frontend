79e0e6d55e0d68b290ee5d99ded7a4e86884220c
Julio Litwin <j.litwin@live.com>
2020-10-23 00:24:26 -0300

    Atualizado interface de ui e o game manaager.

---
+++ Vestibulandos/Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+using Utils.Singletons;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    [SerializeField]
+    private Camera m_mainCamera = null;
+
+    public Camera MainCamera => m_mainCamera;
+
+    private void Awake()
+    {
+        Application.runInBackground = true;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+        if (m_mainCamera == null)
+        {
+            var mainCamera = GameObject.Find("Main Camera");
+            if (mainCamera)
+            {
+                // Get the camera component.
+                var cam = mainCamera.GetComponent<UnityEngine.Camera>();
+
+                // Set the camera.
+                m_mainCamera = cam;
+            }
+            else
+            {
+                LoggerHelper.LogError("Failed to find the 'main camera'.");
+                return;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Vestibulandos/Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,34 @@
+﻿public class Singleton<T> where T: new()
+{
+    private static T s_instance = default;
+
+    /// <summary>
+    /// Obter atual instancia.
+    /// </summary>
+    /// <returns></returns>
+    public static T GetInstance()
+    {
+        if (s_instance == null)
+            s_instance = new T();
+
+        return s_instance;
+    }
+
+    /// <summary>
+    /// Destruir a instance.
+    /// </summary>
+    public static void Destroy()
+    {
+        // GC collection
+        s_instance = default;
+    }
+
+    /// <summary>
+    /// Verificar se a isntancia já foi criada.
+    /// </summary>
+    /// <returns></returns>
+    public static bool IsCreated()
+    {
+        return s_instance != null;
+    }
+}
