6ea6eef55cbe946378fdaa27cc35ed4f5de204b9
Julio Litwin <j.litwin@live.com>
2020-10-24 00:56:50 -0300

    Adicionado questão e enumerations da partida e questões.

---
+++ Vestibulandos/Assets/Scripts/Logica/Enums.cs
@@ -0,0 +1,16 @@
+﻿public enum TipoPartida
+{
+    Nenhuma,
+    Treino,
+    Rankeada
+}
+
+public enum AreaConhecimento
+{
+    CienciasHumanas,
+    CienciasDaNatureza,
+    LinguagensCodigos,
+    Matematica,
+
+    Todos,
+}
