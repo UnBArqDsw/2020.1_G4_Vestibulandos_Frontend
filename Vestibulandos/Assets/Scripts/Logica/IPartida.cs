6b5fad19d3e132488f3368069ad0b7b6f041940f
Julio Litwin <j.litwin@live.com>
2020-10-24 00:56:32 -0300

    Adicionado primeira classes de partida.

---
+++ Vestibulandos/Assets/Scripts/Logica/IPartida.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public interface IPartida
+{
+    int Id { get; set; }
+    TipoPartida TipoPartida { get; set; }
+    DateTime DataCriado { get; set; }
+    DateTime DataFinalizado { get; set; }
+    int JogadorCriado { get; set; }
+    int Dificuldade { get; set; }
+
+    AreaConhecimento AreaConhecimento { get; set; }
+
+    List<IQuestao> Questao { get; set; }
+    int QuantidadeQuestao { get; set; }
+    bool Revisao { get; set; }
+}
---
+++ Vestibulandos/Assets/Scripts/Logica/Treino.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treino : MonoBehaviour, IPartida
+{
+    public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public TipoPartida TipoPartida { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public DateTime DataCriado { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public DateTime DataFinalizado { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int JogadorCriado { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int Dificuldade { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public AreaConhecimento AreaConhecimento { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public List<IQuestao> Questao { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int QuantidadeQuestao { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public bool Revisao { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
