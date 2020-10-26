f0eb3d63fa44fd2589fa945edc36c5763df95778
Julio Litwin <j.litwin@live.com>
2020-10-26 17:15:16 -0300

    Adicionado o GameManager, principal script do jogo.
    Co-authored-by: Ricardo Canela <ricardolimacanela@gmail.com>
    Co-authored-by: Lucas Gomes <lucas.gomesgs0@gmail.com>

--- Vestibulandos/Assets/Scripts/GameManager.cs
+++ Vestibulandos/Assets/Scripts/GameManager.cs
@@ -1,62 +1,138 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Utils;
-using Utils.Singletons;
 
 public class GameManager : MonoSingleton<GameManager>
 {
     [SerializeField]
-    private Camera m_mainCamera = null;
+    private List<QuestaoScriptableObject> m_listQuestoesData;
 
-    public Camera MainCamera => m_mainCamera;
+    [SerializeField]
+    private float m_nTempo;
 
-    private void Awake()
+    private string m_strAtualCategoria = "";
+
+    private int m_nAcertoQuestao = 0;
+
+    private List<IQuestao> m_listQuestoes = new List<IQuestao>();
+
+    private IQuestao m_atualQuestao;
+
+    private int m_nPontuacao;
+    private int m_nVida;
+    private float m_fAtualTempo;
+
+    private QuestaoScriptableObject m_scriptableQuestoa;
+
+    private JogoEstado m_enEstado = JogoEstado.Proximo;
+
+    public JogoEstado Estado => m_enEstado;
+
+    public List<QuestaoScriptableObject> QuizData { get => m_listQuestoesData; }
+
+    public void Iniciar(int categoriaId, string categoria)
     {
-        Application.runInBackground = true;
-        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        m_strAtualCategoria = categoria;
+        m_nAcertoQuestao = 0;
+        m_nPontuacao = 0;
 
-        if (m_mainCamera == null)
+        m_nVida = 3;
+
+        m_fAtualTempo = m_nTempo;
+
+        m_scriptableQuestoa = m_listQuestoesData[categoriaId];
+        m_listQuestoes.AddRange(m_scriptableQuestoa.Questoes);
+
+        SelecionarQuestao();
+        m_enEstado = JogoEstado.Jogando;
+    }
+
+    private void SelecionarQuestao()
+    {
+        int randonizado = UnityEngine.Random.Range(0, m_listQuestoes.Count);
+        m_atualQuestao = m_listQuestoes[randonizado];
+
+        UIManager.Instance.AdicionarQuestao(m_atualQuestao);
+
+        m_listQuestoes.RemoveAt(randonizado);
+    }
+
+    private void Update()
+    {
+        if (m_enEstado != JogoEstado.Jogando)
+            return;
+
+        m_fAtualTempo -= Time.deltaTime;
+        AtualizarTempo(m_fAtualTempo);
+    }
+
+    void AtualizarTempo(float value)
+    {
+        UIManager.Instance.Tempo.text = TimeSpan.FromSeconds(m_fAtualTempo).ToString("mm':'ss");
+        if (m_fAtualTempo <= 0) Fim();
+    }
+
+    public bool Resposta(string opcaoSelecionada)
+    {
+        bool correta = false;
+
+        switch (m_atualQuestao.TipoQuestao)
         {
-            var mainCamera = GameObject.Find("Main Camera");
-            if (mainCamera)
-            {
-                // Get the camera component.
-                var cam = mainCamera.GetComponent<UnityEngine.Camera>();
+            case TipoQuestao.C:
+                {
+                    QuestaoC c = m_atualQuestao as QuestaoC;
 
-                // Set the camera.
-                m_mainCamera = cam;
+                    if (c.RespostaCorreta == opcaoSelecionada)
+                    {
+                        m_nAcertoQuestao++;
+
+                        correta = true;
+                        m_nPontuacao += 1;
+
+                        UIManager.Instance.Pontuacao.text = (m_nPontuacao > 1) ? $"Ponto: {m_nPontuacao}" : $"Pontos: {m_nPontuacao}";
+
+                    }
+                    else
+                    {
+                        m_nVida--;
+
+                        UIManager.Instance.ReduzirVida(m_nVida);
+
+                        if (m_nVida == 0)
+                        {
+                            Fim();
+                        }
+                    }
+                }
+                break;
+            default:
+                {
+                    UnityEngine.Debug.LogError($"Tipo de questao não adicionado. Tipo: {m_atualQuestao.TipoQuestao}.");
+                }
+                break;
+        }
+
+        if (m_enEstado == JogoEstado.Jogando)
+        {
+            if (m_listQuestoes.Count > 0)
+            {
+                Invoke("SelecionarQuestao", 0.45f);
             }
             else
             {
-                LoggerHelper.LogError("Failed to find the 'main camera'.");
-                return;
+                Fim();
             }
         }
 
-        Initialize();
+        return correta;
     }
 
-    private bool Initialize()
+    private void Fim()
     {
-        if(!DataManager.GetInstance().LoadData())
-        {
-            LoggerHelper.LogError("Failed to load the data.");
-            return false;
-        }
+        m_enEstado = JogoEstado.Proximo;
+        UIManager.Instance.FimJogo.SetActive(true);
 
-        return true;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        PlayerPrefs.SetInt(m_strAtualCategoria, m_nAcertoQuestao);
     }
 }
