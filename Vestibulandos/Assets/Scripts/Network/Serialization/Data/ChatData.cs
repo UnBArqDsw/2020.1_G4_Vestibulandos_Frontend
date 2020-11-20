7c8f8bde50d8fc6bc4db391edb49da74a28c365a
Julio Litwin <j.litwin@live.com>
2020-11-19 22:29:34 -0300

    Adicionado data dos packets e meta files.

---
+++ Vestibulandos/Assets/Scripts/Network/Serialization/Data/ChatData.cs
@@ -0,0 +1,84 @@
+﻿using Common.Constants;
+using MessagePack;
+
+namespace Serialization.Data
+{
+    [MessagePackObject]
+    public class ChatData
+    {
+        //---------------------------------------------------------------------------------------------------
+        // Member variables.
+        //---------------------------------------------------------------------------------------------------
+        //private ulong m_ulCharacterIndex;
+        //private string m_strCharacterName;
+
+        //private EnGender m_enGender;
+
+        //private int m_iAvatarIndex;
+
+        //private int m_iLevel;
+        //private int m_iVipLevel;
+
+        //private int m_iChatType;
+        //private string m_strMessage;
+
+        [Key(0)]
+        public ulong CharacterIndex { get; set; }
+
+        [Key(1)]
+        public string CharacterName { get; set; }
+
+        [Key(2)]
+        public int /*EnGender*/ Gender { get; set; }
+
+        [Key(3)]
+        public int AvatarIndex { get; set; }
+
+        [Key(4)]
+        public int Level { get; set; }
+
+        [Key(5)]
+        public int VipLevel { get; set; }
+
+        [Key(6)]
+        public int /*EnChatType*/ ChatType { get; set; }
+
+        [Key(7)]
+        public string Message { get; set; }
+
+        //---------------------------------------------------------------------------------------------------
+        // Constructors.
+        //---------------------------------------------------------------------------------------------------
+        //public ChatData(ulong ulCharacterIndex, string strCharacterName, EnGender enGender, int iAvatarIndex, int iLevel, int iVipLevel, int iChatType, string strMessage)
+        //{
+        //    CharacterIndex = ulCharacterIndex;
+        //    CharacterName = strCharacterName;
+
+        //    AvatarIndex = iAvatarIndex;
+
+        //    Gender = enGender;
+
+        //    Level = iLevel;
+        //    VipLevel = iVipLevel;
+
+        //    ChatType = iChatType;
+        //    Message = strMessage;
+        //}
+
+        //---------------------------------------------------------------------------------------------------
+        // Properties.
+        //---------------------------------------------------------------------------------------------------
+        //public ulong CharacterIndex => m_ulCharacterIndex;
+        //public string CharacterName => m_strCharacterName;
+
+        //public EnGender Gender => m_enGender;
+
+        //public int AvatarIndex => m_iAvatarIndex
+
+        //public int Level => m_iLevel;
+        //public int VipLevel => m_iVipLevel;
+
+        //public int ChatType => m_iChatType;
+        //public string Message => m_strMessage;
+    }
+}
---
+++ Vestibulandos/Assets/Scripts/Network/Serialization/Data/ServerData.cs
@@ -0,0 +1,35 @@
+﻿using MessagePack;
+
+namespace Serialization.Data
+{
+    [MessagePackObject]
+    public class ServerData
+    {
+        [Key(0)]
+        public int ServerId { get; set; }
+
+        [Key(1)]
+        public string Name { get; set; }
+
+        [Key(2)]
+        public string ServerIp { get; set; }
+
+        [Key(3)]
+        public int ServerPort { get; set; }
+
+        [Key(4)]
+        public int CurrentUserCount { get; set; }
+
+        [Key(5)]
+        public int MaxUserCount { get; set; }
+
+        [Key(6)]
+        public int Status { get; set; }
+
+        [Key(7)]
+        public bool IsNew { get; set; }
+
+        [Key(8)]
+        public bool IsMaintenance { get; set; }
+    }
+}
